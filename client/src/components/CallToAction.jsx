991144571dbce809eb447a4c55e0a393d130908e
Francisco <frajcastillo@gmail.com>
2024-01-30 23:26:28 -0600

    add call to action component

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Image from "react-bootstrap/esm/Image";
+import Button from "react-bootstrap/esm/Button";
+
+export default function CallToAction() {
+  return (
+    <div style ={{border: "2px solid #5c8ab9", borderRadius: "5px"}}className="flex flex-col sm:flex-row p-3 justify-center items-center"
+
+    >
+      <div className="flex-1 justify-center flex flex-col">
+        <h2 className="text-2xl">Want to learn more about Texas Wesleyan?</h2>
+        <p className="text-muted">Check out the offical website of Texas Wesleyan!</p>
+        <Button style={{background: "#0072bc"}}>Learn more</Button>
+      </div>
+      <div className="p-7 flex-1">
+        <Image src="https://th.bing.com/th/id/OIP.NgphJPEz_Zu5gJwVt9yp3AHaEK?w=321&h=180&c=7&r=0&o=5&pid=1.7" />
+      </div>
+    </div>
+  );
+}
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -4,6 +4,7 @@ import Spinner from "react-bootstrap/Spinner";
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/esm/Button";
 import Image from "react-bootstrap/esm/Image";
+import CallToAction from "../components/CallToAction";
 
 export default function PostPage() {
   const { postSlug } = useParams();
@@ -61,6 +62,9 @@ export default function PostPage() {
       </div>
       <div className="p-3 max-w-2xl mx-auto w-full" dangerouslySetInnerHTML={{__html: post && post.content}}>
 
+      </div>
+      <div className="max-w-4xl mx-auto w-full">
+        <CallToAction/>
       </div>
     </main>
   );
